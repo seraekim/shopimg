9ad2b586bcf92e3ff8e87192452d68702b51ee90
Serae Kim <kserae@gmail.com>
2017-05-01 01:44:29 +0900

    img scroll

--- src/main/webapp/WEB-INF/views/shopImg/search-imgscroll.jsp
+++ src/main/webapp/WEB-INF/views/shopImg/search-imgscroll.jsp
@@ -2,6 +2,103 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
   
   <link href="resources/css/search-imgscroll.css" rel="stylesheet">
-<!--   <div id="visualization"></div> -->
-  <ul id="imgscroll"></ul>
+  <header class="cd-header">
+    <h1>Content Filters</h1>
+  </header>
+
+  <div class="cd-main-content">
+    <div class="cd-tab-filter-wrapper">
+      <div class="cd-tab-filter">
+        <ul class="cd-filters">
+          <li class="placeholder">
+            <a data-type="all" href="#0" onclick="return false;" >All</a>
+            <!-- selected option on mobile -->
+          </li>
+          <li class="filter"><a class="selected" href="#0" onclick="return false;" data-type="all">All</a></li>
+          <li class="filter" data-filter=".20170410"><a href="#0" onclick="return false;" data-type="20170410">20170410</a></li>
+          <li class="filter" data-filter=".color-2"><a href="#0" onclick="return false;" data-type="color-2">Color 2</a></li>
+        </ul>
+        <!-- cd-filters -->
+      </div>
+      <!-- cd-tab-filter -->
+    </div>
+    <!-- cd-tab-filter-wrapper -->
+
+    <section class="cd-gallery">
+      <div class="cd-fail-message" style="display: none;"></div>
+      <ul></ul>
+    </section>
+    <!-- cd-gallery -->
+
+    <div class="cd-filter">
+      <form>
+        <div class="cd-filter-block">
+          <h4>Search</h4>
+
+          <div class="cd-filter-content">
+            <input type="search" placeholder="Try color-1...">
+          </div>
+          <!-- cd-filter-content -->
+        </div>
+        <!-- cd-filter-block -->
+
+        <div class="cd-filter-block">
+          <h4>Check boxes</h4>
+
+          <ul class="cd-filter-content cd-filters list">
+          </ul>
+          <!-- cd-filter-content -->
+        </div>
+        <!-- cd-filter-block -->
+
+        <div class="cd-filter-block">
+          <h4>Select</h4>
+
+          <div class="cd-filter-content">
+            <div class="cd-select cd-filters">
+              <select class="filter" name="selectThis" id="selectThis">
+                <option value="">Choose an option</option>
+                <option value=".option1">Option 1</option>
+                <option value=".option2">Option 2</option>
+                <option value=".option3">Option 3</option>
+                <option value=".option4">Option 4</option>
+              </select>
+            </div>
+            <!-- cd-select -->
+          </div>
+          <!-- cd-filter-content -->
+        </div>
+        <!-- cd-filter-block -->
+
+        <div class="cd-filter-block">
+          <h4>Radio buttons</h4>
+
+          <ul class="cd-filter-content cd-filters list">
+            <li>
+              <input class="filter" data-filter="" type="radio" name="radioButton" id="radio1" checked>
+              <label class="radio-label" for="radio1">All</label>
+            </li>
+
+            <li>
+              <input class="filter" data-filter=".radio2" type="radio" name="radioButton" id="radio2">
+              <label class="radio-label" for="radio2">Choice 2</label>
+            </li>
+
+            <li>
+              <input class="filter" data-filter=".radio3" type="radio" name="radioButton" id="radio3">
+              <label class="radio-label" for="radio3">Choice 3</label>
+            </li>
+          </ul>
+          <!-- cd-filter-content -->
+        </div>
+        <!-- cd-filter-block -->
+      </form>
+
+      <a href="#0" onclick="return false;" class="cd-close">Close</a>
+    </div>
+    <!-- cd-filter -->
+
+    <a href="#0" onclick="return false;" class="cd-filter-trigger">Filters</a>
+  </div>
+  <script src="resources/lib/mixitup/jquery.mixitup.min.js"></script>
   <script src="resources/js/search-imgscroll.js"></script>
--- src/main/webapp/resources/js/search-imgscroll.js
+++ src/main/webapp/resources/js/search-imgscroll.js
@@ -1,5 +1,7 @@
-(function() {
-  $.fn.srkimImgScroll = function(options) {
+var failCnt = 0;
+
+(function () {
+  $.fn.srkimImgScroll = function (options) {
 
     console.log(options);
 
@@ -13,21 +15,23 @@
     console.log(config);
 
     var ul = this;
-    var show = function(li) {
+    var show = function (li) {
       if ($(window).scrollTop() + $(window).height() > $(li).offset().top) {
-        $(li).addClass('show');
+        //$(li).addClass('show2');
       } else if ($(window).scrollTop() + $(window).height() < $(li).offset().top) {
         if (config.repeatShow) {
-          $(li).removeClass('show');
+          //$(li).removeClass('show2');
         }
       }
     }
-    var refresh = function() {
+    var refresh = function () {
       if (ul.length <= 0) {
         return;
       }
 
-      ul.css({"position": "relative"});
+      ul.css({
+        "position": "relative"
+      });
 
       var lis = $(ul).children("li");
 
@@ -47,7 +51,10 @@
       if (ul_column > 0) {
         $(ul).removeClass('min');
 
-        lis.css({"position": "absolute", "width": config.width});
+        lis.css({
+          "position": "absolute",
+          "width": config.width
+        });
 
         var maxHeight = 0;
         var list = []
@@ -116,73 +123,295 @@
   }
 })();
 
+$(document).ready(function () {
+  /*$('.cd-gallery ul').srkimImgScroll({
+    width: 360,
+    delay: 200
+  });*/
+  
+  var loading = false;
+  var dist = 300;
+  var num = 0;
+  var imgList = [];
 
-$(document).ready(function() {
-	$('#imgscroll').srkimImgScroll({
-		width : 360,
-		delay : 200
-	});
+
+  $.ajax({
+  	type : "POST",
+  	url : ctxp + "/shopImg/search-timeline",
+  	dataType : 'json',
+  	data : $('#form_search').serialize()
+  }).done(function(res) {
+  	console.log(res);
+
+  	var itemList = [];
+  	/*for ( var i in res.img) {
+  		var o = res.img[i];
+  		var start = o.d.substring(0, 4) + '-' + o.d.substring(4, 6) + '-' + o.d.substring(6, 8) + ' ' + o.h + ':00:00';
+  		var content = o.sId;
+  		var sNm = o.sNm;
+  		var imgUrl = o.img;
+  		var cId = o.cId;
+  		itemList.push({
+  			id : o.d + o.h + cId,
+  			content : sNm + o.h,
+  			start : start,
+  			group : cId,
+  			'img' : imgUrl
+  		});
+  	}*/
+  	for (var i in res.img) {
+  		var o = res.img[i];
+  		imgList.push(o.img+'.win.jpg');
+  		var imgStrArr = o.img.split('.');
+  	}
+  	
+  	for(var i in res.date) {
+  		$('.cd-filter-block ul').append('<li><input class="filter" data-filter=".d'+res.date[i]+'" type="checkbox" id="checkbox'+i+'">'+
+  				'<label class="checkbox-label" for="checkbox'+i+'">'+res.date[i]+'</label></li>');
+  	}
+  	
+    setInterval(function () {
+      if ((failCnt == 0 || $('.cd-fail-message').css('display') == 'none') && $(window).scrollTop() >= $(document).height() - $(window).height() - dist 
+    		  && !loading && num < res.img.length) {
+        loading = true;
+        $(".cd-gallery ul").append('<li class="mix d'+imgList[num].split('.')[1].substring(0,8)+'"><div><img src="img/' + imgList[num] + '"></div></li>');
+//        if (num == 9) {
+//          num = 1;
+//        } else {
+          num++;
+//        }
+        loading = false;
+        $('.filter .selected').trigger('click');
+      }
+    }, 200);
+    
+    setTimeout(function(){
+      lazyLoading();
+    }, 1000);
+  }).fail(function(xhr, status, e) {
+  	console.log(xhr, status, e);
+  });
+  
 });
 
-$('#imgscroll').on('click', 'li.show div', function() {
-	var $src = $(this).children('img').attr('src');
-	window.open($src, '_blank');
-	console.log($(this).index(), $src);
-});
-/*
-function random(min, max) {
-	return min + Math.floor(Math.random() * (max - min + 1))
+var lazyLoading = function () {
+  $('.cd-fail-message').text('No results found');
+  
+  //open/close lateral filter
+  $('.cd-filter-trigger').on('click', function () {
+    triggerFilter(true);
+  });
+  $('.cd-filter .cd-close').on('click', function () {
+    triggerFilter(false);
+  });
+
+  function triggerFilter($bool) {
+    console.log($bool);
+    var elementsToTrigger = $([$('.cd-filter-trigger'), $('.cd-filter'), $('.cd-tab-filter'), $('.cd-gallery')]);
+    elementsToTrigger.each(function () {
+      $(this).toggleClass('filter-is-visible', $bool);
+    });
+  }
+
+  //mobile version - detect click event on filters tab
+  var filter_tab_placeholder = $('.cd-tab-filter .placeholder a'),
+    filter_tab_placeholder_default_value = 'Select',
+    filter_tab_placeholder_text = filter_tab_placeholder.text();
+  
+  var selected_filter;
+  $('.cd-tab-filter li').on('click', function (event) {
+    //detect which tab filter item was selected
+    var imsi = $(event.target).data('type');
+    if(selected_filter == imsi && $('html').css('overflow') == 'hidden') {
+      return;
+    } else {
+      selected_filter = imsi;
+    }
+    console.log(selected_filter);
+    //check if user has clicked the placeholder item
+    if ($(event.target).is(filter_tab_placeholder)) {
+      (filter_tab_placeholder_default_value == filter_tab_placeholder.text()) ? filter_tab_placeholder.text(filter_tab_placeholder_text): filter_tab_placeholder.text(filter_tab_placeholder_default_value);
+      $('.cd-tab-filter').toggleClass('is-open');
+
+      //check if user has clicked a filter already selected 
+    } else if (filter_tab_placeholder.data('type') == selected_filter) {
+      filter_tab_placeholder.text($(event.target).text());
+      $('.cd-tab-filter').removeClass('is-open');
+
+    } else {
+      //close the dropdown and change placeholder text/data-type value
+      $('.cd-tab-filter').removeClass('is-open');
+      filter_tab_placeholder.text($(event.target).text()).data('type', selected_filter);
+      filter_tab_placeholder_text = $(event.target).text();
+
+      //add class selected to the selected filter item
+      $('.cd-tab-filter .selected').removeClass('selected');
+      $(event.target).addClass('selected');
+    }
+  });
+
+  //close filter dropdown inside lateral .cd-filter 
+  $('.cd-filter-block h4').on('click', function () {
+    $(this).toggleClass('closed').siblings('.cd-filter-content').slideToggle(300);
+  })
+
+  //fix lateral filter and gallery on scrolling
+  $(window).on('scroll', function () {
+    (!window.requestAnimationFrame) ? fixGallery(): window.requestAnimationFrame(fixGallery);
+  });
+
+  function fixGallery() {
+    var offsetTop = $('.cd-main-content').offset().top,
+      scrollTop = $(window).scrollTop();
+    (scrollTop >= offsetTop) ? $('.cd-main-content').addClass('is-fixed'): $('.cd-main-content').removeClass('is-fixed');
+  }
+
+  /************************************
+  	MitItUp filter settings
+  	More details: 
+  	https://mixitup.kunkalabs.com/
+  	or:
+  	http://codepen.io/patrickkunka/
+  *************************************/
+
+  buttonFilter.init();
+  $('.cd-gallery ul').mixItUp({
+    controls: {
+      enable: false
+    },
+    callbacks: {
+      onMixStart: function () {
+//        $('.cd-fail-message').fadeOut(200);
+        $('.cd-fail-message').hide();
+        $('html').attr('style','overflow:""');
+      },
+      onMixFail: function () {
+//        $('.cd-fail-message').fadeIn(200);
+        $('.cd-fail-message').show();
+        $('html').attr('style','overflow:hidden');
+        failCnt++;
+      }
+    }
+  });
+
+  //search filtering
+  //credits http://codepen.io/edprats/pen/pzAdg
+  var inputText;
+  var $matching = $();
+
+  var delay = (function () {
+    var timer = 0;
+    return function (callback, ms) {
+      clearTimeout(timer);
+      timer = setTimeout(callback, ms);
+    };
+  })();
+
+  $(".cd-filter-content input[type='search']").keyup(function () {
+    // Delay function invoked to make sure user stopped typing
+    delay(function () {
+      inputText = $(".cd-filter-content input[type='search']").val().toLowerCase();
+      // Check to see if input field is empty
+      if ((inputText.length) > 0) {
+        $('.mix').each(function () {
+          var $this = $(this);
+
+          // add item to be filtered out if input text matches items inside the title   
+          if ($this.attr('class').toLowerCase().match(inputText)) {
+            $matching = $matching.add(this);
+          } else {
+            // removes any previously matched item
+            $matching = $matching.not(this);
+          }
+        });
+        $('.cd-gallery ul').mixItUp('filter', $matching);
+      } else {
+        // resets the filter to show all item if input is empty
+        $('.cd-gallery ul').mixItUp('filter', 'all');
+      }
+    }, 200);
+  });
 }
+/*****************************************************
+	MixItUp - Define a single object literal 
+	to contain all filter custom functionality
+*****************************************************/
+var buttonFilter = {
+  // Declare any variables we will need as properties of the object
+  $filters: null,
+  groups: [],
+  outputArray: [],
+  outputString: '',
 
-function pad(num, size) {
-	return ('0000' + num).substr(-size);
-}*/
-var loading = false;
-var dist = 500;
-var num = 0;
-var imgList = [];
+  // The "init" method will run on document ready and cache any jQuery objects we will need.
+  init: function () {
+    var self = this; // As a best practice, in each method we will asign "this" to the variable "self" so that it remains scope-agnostic. We will use it to refer to the parent "buttonFilter" object so that we can share methods and properties between all parts of the object.
 
+    self.$filters = $('.cd-main-content');
+    self.$container = $('.cd-gallery ul');
 
-$.ajax({
-	type : "POST",
-	url : ctxp + "/shopImg/search-timeline",
-	dataType : 'json',
-	data : $('#form_search').serialize()
-}).done(function(res) {
-	console.log(res);
+    self.$filters.find('.cd-filters').each(function () {
+      var $this = $(this);
 
-	var itemList = [];
-	/*for ( var i in res.img) {
-		var o = res.img[i];
-		var start = o.d.substring(0, 4) + '-' + o.d.substring(4, 6) + '-' + o.d.substring(6, 8) + ' ' + o.h + ':00:00';
-		var content = o.sId;
-		var sNm = o.sNm;
-		var imgUrl = o.img;
-		var cId = o.cId;
-		itemList.push({
-			id : o.d + o.h + cId,
-			content : sNm + o.h,
-			start : start,
-			group : cId,
-			'img' : imgUrl
-		});
-	}*/
-	for (var i in res.img) {
-		var o = res.img[i];
-		imgList.push(o.img);
-	}
-	setInterval(function() {
-		if ($(window).scrollTop() >= $(document).height() - $(window).height() - dist && !loading) {
-			loading = true;
-			$("#imgscroll").append('<li><div><img src="img/' + imgList[num] + '"></div></li>');
-//			if (num == 14) {
-//				num = 0;
-//			} else {
-				num++;
-//			}
-			loading = false;
-		}
-	}, 200);
-}).fail(function(xhr, status, e) {
-	console.log(xhr, status, e);
-});
+      self.groups.push({
+        $inputs: $this.find('.filter'),
+        active: '',
+        tracker: false
+      });
+    });
+
+    self.bindHandlers();
+  },
+
+  // The "bindHandlers" method will listen for whenever a button is clicked. 
+  bindHandlers: function () {
+    var self = this;
+
+    self.$filters.on('click', 'a', function (e) {
+      self.parseFilters();
+    });
+    self.$filters.on('change', function () {
+      self.parseFilters();
+    });
+  },
+
+  parseFilters: function () {
+    var self = this;
+
+    // loop through each filter group and grap the active filter from each one.
+    for (var i = 0, group; group = self.groups[i]; i++) {
+      group.active = [];
+      group.$inputs.each(function () {
+        var $this = $(this);
+        if ($this.is('input[type="radio"]') || $this.is('input[type="checkbox"]')) {
+          if ($this.is(':checked')) {
+            group.active.push($this.attr('data-filter'));
+          }
+        } else if ($this.is('select')) {
+          group.active.push($this.val());
+        } else if ($this.find('.selected').length > 0) {
+          group.active.push($this.attr('data-filter'));
+        }
+      });
+    }
+    self.concatenate();
+  },
+
+  concatenate: function () {
+    var self = this;
+
+    self.outputString = ''; // Reset output string
+
+    for (var i = 0, group; group = self.groups[i]; i++) {
+      self.outputString += group.active;
+    }
+
+    // If the output string is empty, show all rather than none:    
+    !self.outputString.length && (self.outputString = 'all');
+
+    // Send the output string to MixItUp via the 'filter' method:    
+    if (self.$container.mixItUp('isLoaded')) {
+      self.$container.mixItUp('filter', self.outputString);
+    }
+  }
+};
